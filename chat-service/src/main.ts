900b19c8132f77f81dffd24127616c3373b539c4
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-09 02:07:01 +0400

    Add Kafka microservice integration to chat-service in main.ts, enabling message handling and communication.

--- chat-service/src/main.ts
+++ chat-service/src/main.ts
@@ -6,9 +6,26 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'chat-service',
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'chat-service-consumer',
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3004;
