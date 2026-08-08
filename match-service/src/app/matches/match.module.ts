d42f75e0df8a92bdbbe4fee5cbd8c6f7c6960251
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-08 14:32:21 +0400

    Register MatchService in MatchModule to enhance match handling capabilities.

--- match-service/src/app/matches/match.module.ts
+++ match-service/src/app/matches/match.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MatchResolver } from './match.resolver';
+import { MatchService } from './match.service';
 
 @Module({
-  providers: [MatchResolver],
+  providers: [MatchResolver, MatchService],
 })
 export class MatchModule {}
