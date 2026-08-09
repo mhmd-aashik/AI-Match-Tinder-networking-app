984cb563d26dd91c15c4d3cdddde56a43079cef4
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-09 09:45:15 +0400

    Implement user authentication in UserServiceClient; enhance error handling for user creation process via GraphQL.

---
+++ api-gateway/src/app/users/dto/create-user.input.ts
@@ -0,0 +1,25 @@
+import { Field, InputType } from '@nestjs/graphql';
+
+@InputType()
+export class CreateUserInput {
+  @Field()
+  displayName!: string;
+
+  @Field({ nullable: true })
+  bio?: string;
+
+  @Field()
+  dateOfBirth!: string;
+
+  @Field({ nullable: true })
+  gender?: string;
+
+  @Field({ nullable: true })
+  city?: string;
+
+  @Field({ nullable: true })
+  country?: string;
+
+  @Field({ nullable: true })
+  profileImageUrl?: string;
+}
