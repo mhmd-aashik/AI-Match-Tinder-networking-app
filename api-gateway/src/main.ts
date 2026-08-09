b1ddda94ffda272d51847bac3e09b1303b86c8bc
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-10 02:20:05 +0400

    Update main.ts to enable CORS for localhost and change default port to 4000 for development environment.

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,18 +1,22 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+
+  const port = process.env.PORT || 4000;
+
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
   );
