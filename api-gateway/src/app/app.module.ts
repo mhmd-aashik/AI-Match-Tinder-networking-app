1e9062f997f2c2578a212ceb5391b0cf88367f83
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-09 09:17:54 +0400

    Integrate GraphQL support into the application by adding GraphQLModule with ApolloDriver; include GatewayModule in the imports for enhanced functionality.

--- api-gateway/src/app/app.module.ts
+++ api-gateway/src/app/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GatewayModule } from './gateway/gateway.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'api-gateway/src/app/graphql/schema.gql',
+      graphiql: true,
+    }),
+    AuthModule,
+    GatewayModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
