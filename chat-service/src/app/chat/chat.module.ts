3235c458121f52f04ab0dacd831d98a99eb3d681
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-09 08:59:39 +0400

    Import PassportModule into ChatModule to enable authentication features for chat functionality.

--- chat-service/src/app/chat/chat.module.ts
+++ chat-service/src/app/chat/chat.module.ts
@@ -4,8 +4,10 @@ import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 import { SocketAuthService } from '../auth/socket-auth.service';
 import { ChatResolver } from './chat.resolver';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [PassportModule],
   providers: [ChatGateway, ChatService, SocketAuthService, ChatResolver],
 })
 export class ChatModule {}
