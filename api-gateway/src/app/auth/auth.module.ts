d27fc503c1a4122a4cedb31e9abf5aff981f0346
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-09 10:03:52 +0400

    Enhance AuthModule by adding GqlAuthGuard to providers and exports for improved GraphQL authentication support.

--- api-gateway/src/app/auth/auth.module.ts
+++ api-gateway/src/app/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { PassportModule } from '@nestjs/passport';
 import { KeycloakStrategy } from './keycloak.strategy';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { RolesGuard } from './roles.guard';
+import { GqlAuthGuard } from './gql-auth.guard';
 
 @Module({
   imports: [PassportModule],
-  providers: [KeycloakStrategy, JwtAuthGuard, RolesGuard],
-  exports: [JwtAuthGuard],
+  providers: [KeycloakStrategy, JwtAuthGuard, RolesGuard, GqlAuthGuard],
+  exports: [JwtAuthGuard, GqlAuthGuard, RolesGuard],
 })
 export class AuthModule {}
